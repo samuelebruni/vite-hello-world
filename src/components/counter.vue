907a47586b7f5d184df68083572aac5d81bde4f0
samuelebruni <samuele_bruni@hotmail.com>
2023-09-21 16:09:40 +0200

    end exercise

---
+++ src/components/counter.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <h1>La mia prima APP con Vite ☝</h1>
+    <button type="button" @click="count++">Clicca sul mio Contatore {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Customizza il tuo jumbotron</h1>
+            <p class="col-md-8 fs-4">Descrizione JUMBOTRON</p>
+        </div>
+    </div>
+</template>
+
+<style></style>
